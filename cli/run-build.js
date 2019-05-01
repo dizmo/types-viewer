391511e8e43115006dd4108b36e382f3e2f2d7bf
Hasan Karahan <hasan.karahan@blackhan.com>
2019-05-01 19:14:31 +0300

    yo @dizmo/module --upgrade

--- cli/run-build.js
+++ cli/run-build.js
@@ -1,13 +1,17 @@
 const { arg, npm, npx } = require('./run-utils');
 const { exit } = require('process');
 
-const build = () => npx('tsc').then(() =>
-    npx('babel', '--presets=env', '-qsd', 'dist', 'dist')
+const build = () => npx('tsc').then(() => npx('babel',
+    '--presets=env', '--ignore=*.min.js,*.umd.js',
+    '--source-maps=true', '--quiet',
+    '--out-dir', 'dist', 'dist')
 );
 if (require.main === module) {
     let p = npm('install').then(() => {
         p = arg('lint')(true) ? p.then(require('./run-lint')) : p;
-        p.then(build).catch(exit);
+        p = p.then(build);
+        p = arg('prepack')(false) ? p.then(require('./run-prepack')) : p;
+        p = p.catch(exit);
     });
 }
 module.exports = build;
